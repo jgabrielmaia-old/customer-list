146d622dba6504f7a3853c186a042fb69ef11fe0
J. Gabriel Maia <jonagabriel@gmail.com>
2020-08-25 22:45:27 -0300

    Initializing user service

---
+++ src/core/services/user/user.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { UserData } from 'src/app/data/user-data';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor() { }
+
+  postUserDataForm(userData: UserData){
+
+  }
+}
