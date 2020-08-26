3f739d890594d2b3348ee5fc3d6edb749172e75b
J. Gabriel Maia <jonagabriel@gmail.com>
2020-08-25 22:51:52 -0300

    Creating observable post method for user service

--- src/app/user-data-form/user-data-form.component.ts
+++ src/app/user-data-form/user-data-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserData } from './../data/user-data';
 import { NgForm } from '@angular/forms';
+import { UserService } from '../../core/services/user/user.service' ;
 
 @Component({
   selector: 'app-user-data-form',
@@ -21,13 +22,17 @@ export class UserDataFormComponent implements OnInit {
     detail: ``
   };
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
   }
 
   onSubmit(form:NgForm) {
     console.log('Submitted: ', form.valid);
+    this.userService.postUserDataForm(this.userData).subscribe(
+      result => console.log('Success! ', result),
+      error => console.log('Error! ', error)
+    );
   }
 
 }
--- src/core/services/user/user.service.ts
+++ src/core/services/user/user.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { UserData } from 'src/app/data/user-data';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,7 @@ export class UserService {
 
   constructor() { }
 
-  postUserDataForm(userData: UserData){
-
+  postUserDataForm(userData: UserData) : Observable<UserData>{
+    return of(userData);
   }
 }
