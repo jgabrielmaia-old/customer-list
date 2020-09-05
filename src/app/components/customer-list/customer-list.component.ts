1a4a5b613675e4025fecb1a159f04161f1b9b91f
J. Gabriel Maia <jonagabriel@gmail.com>
2020-09-05 11:31:13 -0300

    Filter by customer name

--- src/app/components/customer-list/customer-list.component.ts
+++ src/app/components/customer-list/customer-list.component.ts
@@ -9,13 +9,37 @@ import { CustomerService } from 'src/core/services/customer/customer.service';
   styleUrls: ['./customer-list.component.css']
 })
 export class CustomerListComponent implements OnInit {
+  customers : CustomerData[];
+  filteredCustomers: CustomerData[];
 
-  customers$ : Observable<CustomerData[]>;
+  private _listFilter: string;
 
   constructor(private customerService: CustomerService) { }
 
   ngOnInit(): void {
-    this.customers$ = this.customerService.getAll();
+    this.customerService.getAll().subscribe(
+      (customers: CustomerData[]) => {
+        this.customers = customers;
+        this.performFilter(this._listFilter);
+      }
+    );
+  }
+  
+  get searchFilter(): string {
+    return this._listFilter;
   }
 
+  set searchFilter(value: string){
+    this._listFilter = value;
+    this.performFilter(value);
+  }
+
+  performFilter(filterBy?: string){
+    if(filterBy) {
+      this.filteredCustomers = this.customers.filter((customer: CustomerData) => 
+          customer.name.toLowerCase().indexOf(filterBy.toLowerCase()) !== -1);
+    } else { 
+      this.filteredCustomers = this.customers;
+    }
+  }
 }
